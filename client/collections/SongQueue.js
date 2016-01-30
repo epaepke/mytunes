fabbc0cb6bdf07c8d6fa7c75aff77eb94303d77f
epaepke <epaepke@gmail.com>
2016-01-30 10:08:00 -0800

    Removes song from queue once it has finished playing

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,21 +1,15 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  // playFirst: function(){
-  //   // console.log('called for first song');
-  //   // console.log('this songqueue ', this.models[0]);
-  //   this.models[0].play();
-  //   // this.playerView.setSong(model.get('currentSong'));
-  // },
-
   initialize: function() {
     this.on('add', function(){
-      console.log('actually working');
       this.length ===1 && this.playFirst();
     });
+
+    this.on('ended', function() {
+      this.remove(this.models[0]);
+    });
   }
-
-
 });
 
 SongQueue.prototype.playFirst = function(){
